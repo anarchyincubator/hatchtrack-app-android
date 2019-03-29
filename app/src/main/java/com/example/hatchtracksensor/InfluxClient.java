a393e0a88edaf4cdda6da3a65de82ab504c11601
Michael Reutman <mreutman@gmail.com>
2019-03-28 21:56:23 -0700

    Able to parse InfluxDB JSON query.

--- app/src/main/java/com/example/hatchtracksensor/InfluxClient.java
+++ app/src/main/java/com/example/hatchtracksensor/InfluxClient.java
@@ -2,8 +2,13 @@ package com.example.hatchtracksensor;
 
 import android.util.Log;
 import com.amazonaws.util.IOUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -16,12 +21,12 @@ public class InfluxClient {
     private String mdbName;
 
     public class InfluxMeasurement {
-        public long unixTimestamp;
-        public float temperature;
-        public float humidity;
+        public String timestamp;
+        public double temperature;
+        public double humidity;
 
         public InfluxMeasurement() {
-            unixTimestamp = 0;
+            timestamp = "";
             temperature = 0;
             humidity = 0;
         }
@@ -65,9 +70,73 @@ public class InfluxClient {
     }
 
     private void parseMeasurement(InputStream in) {
+        /*
+         * Parses something that looks like the following...
+         * {
+         *   "results": [
+         *     {
+         *       "statement_id": 0,
+         *       "series": [
+         *         {
+         *           "name": "peep",
+         *           "tags": {
+         *             "hatch_uuid": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+         *             "peep_uuid": "425e11b3-5844-4626-b05a-219d9751e5ca"
+         *           },
+         *           "columns": [
+         *             "time",
+         *             "humidity",
+         *             "temperature"
+         *           ],
+         *           "values": [
+         *             [
+         *               "2019-03-21T03:21:22Z",
+         *               28.61,
+         *               22.57
+         *             ]
+         *           ]
+         *         }
+         *       ]
+         *     }
+         *   ]
+         * }
+         */
+
         try {
+            InfluxMeasurement influxMeasurement = new InfluxMeasurement();
             String data = IOUtils.toString(in);
-            Log.i("MREUTMAN!!!!!!!!!!!!!!!", data);
+            JSONObject reader = new JSONObject(data);
+            JSONArray results = reader.getJSONArray("results");
+            JSONObject object;
+            String test;
+
+            for (int i = 0; i < results.length(); i++) {
+                object = results.getJSONObject(i);
+                JSONArray series = object.getJSONArray("series");
+
+                for (int j = 0; j < series.length(); j++) {
+                    object = series.getJSONObject(j);
+                    JSONArray columns = object.getJSONArray("columns");
+                    JSONArray values = object.getJSONArray("values");
+
+                    for (int k = 0; k < values.length(); k++) {
+                        JSONArray value = values.getJSONArray(k);
+
+                        for (int l = 0; l < columns.length(); l++) {
+                            if (columns.getString(l).equals("time")) {
+                                influxMeasurement.timestamp = value.getString(l);
+                            }
+                            else if (columns.getString(l).equals("temperature")) {
+                                influxMeasurement.temperature = value.getDouble(l);
+                            }
+                            else if (columns.getString(l).equals("humidity")){
+                                influxMeasurement.humidity = value.getDouble(l);
+                            }
+                        }
+                    }
+                }
+            }
+            Log.i("MREUTMAN", "done!");
         }
         catch (Exception e) {
             e.printStackTrace();
